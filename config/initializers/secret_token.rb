285801190e99685108886db1aac432336c301098
Matt Cosand <matt@cosand.net>
2013-02-14 15:27:55 -0800

    output of 'rails new'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Puck::Application.config.secret_token = '53a22e0b407b99facbd94768c6838d0f9319db082b31ca1487862b3ae0f90d4c33ea13eeadbfc55f222dc1e9b0381d2f7f27ab4c772f9b6831998b2c3aaa6e87'
