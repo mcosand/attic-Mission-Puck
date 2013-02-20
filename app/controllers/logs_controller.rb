48efe16a5222c148365ffa8f3d52d06bb186d515
Matt Cosand <matt@cosand.net>
2013-02-20 01:16:12 -0800

    Mission log will let you add entries. New entries pop up on other windows with highlight and auto-scroll

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -5,6 +5,24 @@ class LogsController < ApplicationController
 		@logs = @mission.logs.all
 	end
 
+	def create
+		@log = @mission.logs.build(params[:log])
+
+		respond_to do |format|
+			if @log.save
+		    logger.info 'In observer handler'
+		    puts 'In observer handler'
+		    broadcast "/logs/new", @log.to_json
+
+				format.html # new.html.erb
+				format.json { render :json => @log }
+			else
+				format.html { render :action => "new" }
+				format.json { render :json => @log.errors, :status => :unprocessable_entity }
+			end
+		end
+	end
+
 	private
 		def find_mission
 			@mission = Mission.find(params[:mission_id])
